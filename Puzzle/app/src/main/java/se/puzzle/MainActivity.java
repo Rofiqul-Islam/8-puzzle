6eb8acf717088a2927cad44ff740cd632d4addbd
Md. Shorifuzzaman <shohan.jess@gmail.com>
2017-04-29 14:10:05 +0600

    Update MainActivity.java

--- Puzzle/app/src/main/java/se/puzzle/MainActivity.java
+++ Puzzle/app/src/main/java/se/puzzle/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         runTimer();
 
     }
+    
     public void shuffle(View view){
         run=false;
 
@@ -52,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         initialization();
         start();
     }
+    
     public void initialization(){
         ImageView zero = (ImageView) findViewById(R.id.zero);
         int resID = getResources().getIdentifier(is[a[0]], "drawable", getPackageName());
@@ -91,8 +93,6 @@ public class MainActivity extends AppCompatActivity {
    public void sort(View view) {
         run=true;
 
-
-
     }
 
     public void start(){
@@ -116,9 +116,9 @@ public class MainActivity extends AppCompatActivity {
 
         handler.post(new Runnable() {
             @Override
+           
             public void run() {
                 if (run == true && !stack.isEmpty()) {
-                    //loop for file wrriting stage by stage
                     String str = stack.pop().toString();
                     str = str.replaceAll(",", "");
                     str = str.replaceAll(" ", "");
@@ -126,15 +126,12 @@ public class MainActivity extends AppCompatActivity {
                     int i;
                     for (i = 1; i < str.length() - 1; i++)
                         s += str.charAt(i);
-                    //System.out.println(s);//After this line file wrriting is started;
-                      /*  for(i=0;i<=8;i++){
-                            arr[i]="";
-                        }*/
+                   
                     for (i = 0; i <= 8; i++) {
                         arr[i] = s.charAt(i) - '0';
                     }
-                    Log.i("shafayat", "oka");
-                    my();
+                   
+                   initialization();
 
                 }
 
@@ -144,42 +141,6 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-    }
-    void my()
-    {
-        ImageView zero = (ImageView) findViewById(R.id.zero);
-        int resID = getResources().getIdentifier(is[arr[0]], "drawable", getPackageName());
-        zero.setImageResource(resID);
-        ImageView one = (ImageView) findViewById(R.id.one);
-        resID = getResources().getIdentifier(is[arr[1]], "drawable", getPackageName());
-        one.setImageResource(resID);
-        ImageView two = (ImageView) findViewById(R.id.two);
-        resID = getResources().getIdentifier(is[arr[2]], "drawable", getPackageName());
-        two.setImageResource(resID);
-
-        ImageView three = (ImageView) findViewById(R.id.three);
-        resID = getResources().getIdentifier(is[arr[3]], "drawable", getPackageName());
-        three.setImageResource(resID);
-
-        ImageView four = (ImageView) findViewById(R.id.four);
-        resID = getResources().getIdentifier(is[arr[4]], "drawable", getPackageName());
-        four.setImageResource(resID);
-
-        ImageView five = (ImageView) findViewById(R.id.five);
-        resID = getResources().getIdentifier(is[arr[5]], "drawable", getPackageName());
-        five.setImageResource(resID);
-
-        ImageView six = (ImageView) findViewById(R.id.six);
-        resID = getResources().getIdentifier(is[arr[6]], "drawable", getPackageName());
-        six.setImageResource(resID);
-
-        ImageView seven = (ImageView) findViewById(R.id.seven);
-        resID = getResources().getIdentifier(is[arr[7]], "drawable", getPackageName());
-        seven.setImageResource(resID);
-
-        ImageView eight = (ImageView) findViewById(R.id.eight);
-        resID = getResources().getIdentifier(is[arr[8]], "drawable", getPackageName());
-        eight.setImageResource(resID);
     }
 
 
